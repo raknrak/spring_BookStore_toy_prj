a30264f72e5885ca187c074940610c61de7a5fdd
Gyeongrak Choi <peter6617@gmail.com>
2024-01-01 23:58:39 +0900

    JS alert 추가

--- src/main/webapp/resources/js/controllers.js
+++ src/main/webapp/resources/js/controllers.js
@@ -7,10 +7,12 @@ function addToCart(action) {
 function removeFromCart(action) {
     document.removeForm.action = action;
     document.removeForm.submit();
+    alert("도서가 장바구니에서 삭제되었습니다!");
     window.location.reload();
 }
 
 function clearCart() {
     document.clearForm.submit();
+    alert("도서가 장바구니에서 삭제되었습니다!");
     window.location.reload();
 }
