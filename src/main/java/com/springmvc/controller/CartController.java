5f3e044ed3fa40192e142076bc2cd13ba5a9a0f6
Gyeongrak Choi <peter6617@gmail.com>
2024-01-01 22:27:55 +0900

    sessionid -> sessionId

--- src/main/java/com/springmvc/controller/CartController.java
+++ src/main/java/com/springmvc/controller/CartController.java
@@ -60,29 +60,29 @@ public class CartController {
     @PutMapping("/add/{bookId}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void addCartByNewItem(@PathVariable String bookId, HttpServletRequest request) {
-        String sessionid = request.getSession(true).getId();
-        Cart cart = cartService.read(sessionid);
+        String sessionId = request.getSession(true).getId();
+        Cart cart = cartService.read(sessionId);
         if(cart== null)
-            cart = cartService.create(new Cart(sessionid));
+            cart = cartService.create(new Cart(sessionId));
         Book book = bookService.getBookById(bookId);
         if(book == null)
             throw new IllegalArgumentException(new BookIdException(bookId));
         cart.addCartItem(new CartItem(book));
-        cartService.update(sessionid, cart);
+        cartService.update(sessionId, cart);
     }
 
     @PutMapping("/remove/{bookId}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void removeCartByItem(@PathVariable String bookId, HttpServletRequest request) {
-        String sessionid = request.getSession(true).getId();
-        Cart cart = cartService.read(sessionid);
+        String sessionId = request.getSession(true).getId();
+        Cart cart = cartService.read(sessionId);
         if(cart== null)
-            cart = cartService.create(new Cart(sessionid));
+            cart = cartService.create(new Cart(sessionId));
         Book book = bookService.getBookById(bookId);
         if(book == null)
             throw new IllegalArgumentException(new BookIdException(bookId));
         cart.removeCartItem(new CartItem(book));
-        cartService.update(sessionid, cart);
+        cartService.update(sessionId, cart);
     }
 
     @DeleteMapping("/{cartId}")
